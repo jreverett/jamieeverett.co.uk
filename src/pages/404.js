9a877df98273b222bef4fb39e5243d85c597c443
Jamie Everett <jreverett2442@gmail.com>
2020-09-29 21:58:57 +0100

    Add initial section, compress video assets, change colour scheme

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,28 +5,41 @@
 */
 
 import React from "react"
+import { Helmet } from "react-helmet"
 import { Link } from "gatsby"
 
 import { Header } from "../components"
-import AnimatedBackground from "../files/BlueGreenOil.mp4"
+import AnimatedBackground from "../files/BlueGreenOil-v2.mp4"
 import BackgroundPoster from "../files/BlueGreenOil.png"
 import "./404.css"
 
 export default function NotFound() {
   return (
-    <div>
-      <Header />
-      <video poster={BackgroundPoster} autoPlay muted playsInline loop>
-        <source src={AnimatedBackground}></source>
-      </video>
-      <div className="e404-container">
-        <p className="e404-number">404</p>
-        <div className="e404-text">
-          <p>
-            Page not found, <Link to="/">head back home.</Link>
-          </p>
+    <>
+      <Helmet>
+        <title>Page not found</title>
+      </Helmet>
+      <div>
+        <Header />
+        <video
+          className="e404-video"
+          poster={BackgroundPoster}
+          autoPlay
+          muted
+          playsInline
+          loop
+        >
+          <source src={AnimatedBackground}></source>
+        </video>
+        <div className="e404-container">
+          <p className="e404-number">404</p>
+          <div className="e404-text">
+            <p>
+              Page not found, <Link to="/">head back home.</Link>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,36 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
+import { Header } from "../components"
+import RedInk from "../files/RedInk-v3.mp4"
 import "./index.css"
 
 export default function Home() {
   return (
-    <section>
-      <header>jamie everett</header>
-    </section>
+    <>
+      <Helmet>
+        <title>Jamie Everett - Portfolio & Blog</title>
+      </Helmet>
+
+      <Header />
+      <video className="welcome-video" autoPlay muted playsInline>
+        <source src={RedInk}></source>
+      </video>
+      <div className="welcome">
+        <p className="welcome-info">
+          <span className="welcome-dropcap">Hi,</span> I'm a full stack
+          computing graduate who loves to create awesome user experiences
+        </p>
+      </div>
+      <section>
+        {/* About Me */}
+        here
+      </section>
+      <section>
+        {/* Personal Projects */}
+        {/* CSS mosaic? */}
+      </section>
+      <section>{/* Contact Me */}</section>
+    </>
   )
 }
