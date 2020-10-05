3eaaabd4976f88a7b7647170982558562dfb4336
Jamie Everett <jreverett2442@gmail.com>
2020-10-06 00:04:44 +0100

    Add missing effect hook

--- src/components/ScrollTop/ScrollTop.jsx
+++ src/components/ScrollTop/ScrollTop.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { RiArrowUpLine } from "react-icons/ri"
 
 import "./ScrollTop.css"
@@ -6,7 +6,7 @@ import "./ScrollTop.css"
 export default function ScrollTop() {
   const [showScroll, setShowScroll] = useState(false)
 
-  const checkScrollTop = () => {
+  const onScroll = () => {
     if (!showScroll && window.pageYOffset > 500) {
       setShowScroll(true)
     } else if (showScroll && window.pageYOffset <= 500) {
@@ -18,7 +18,10 @@ export default function ScrollTop() {
     window.scrollTo({ top: 0, behavior: "smooth" })
   }
 
-  window.addEventListener("scroll", checkScrollTop)
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll)
+    return () => window.removeEventListener("scroll", onScroll)
+  })
 
   return (
     <button
