c0311279cc8268ee39e9b3f2d710c5175fbd4404
Jamie Everett <jreverett2442@gmail.com>
2020-10-15 15:14:00 +0100

    Add missing gatsby react helmet plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,6 +86,7 @@ module.exports = {
         path: path.join(__dirname, `src`, `assets`, `images`),
       },
     },
+    `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
   ],
