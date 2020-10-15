6f4bebba283f6fbd1febefc925592974c7955a04
Jamie Everett <jreverett2442@gmail.com>
2020-10-15 16:24:17 +0100

    Disable video casting for Android browser

--- src/pages/404.js
+++ src/pages/404.js
@@ -20,6 +20,7 @@ export default function NotFound() {
           poster={BackgroundPoster}
           autoPlay
           muted
+          disableRemotePlayback
           playsInline
           loop
         >
