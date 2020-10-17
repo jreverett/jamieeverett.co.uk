cef14d19b20c938730e29f65e2fe875444dc88ef
Jamie Everett <jreverett2442@gmail.com>
2020-10-18 00:51:31 +0100

    Implement suggested form fix
    (https://community.netlify.com/t/no-form-submissions-in-netlify-ui/24264/4?u=jreverett)

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
+import { navigate } from "gatsby"
 import { RiMailSendLine } from "react-icons/ri"
 import { SiLinkedin, SiGithub } from "react-icons/si"
 
 import "./Contact.css"
 
+// encodes captured data from the form in the format required by Netlify
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
 export default function Contact() {
+  const [email, setEmail] = useState("")
+  const [name, setName] = useState("")
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": event.target.getAttribute("name"),
+        email,
+        name,
+        message,
+      }),
+    })
+      .then(() => navigate("/success"))
+      .catch(error => alert("Sorry, your message failed to send: ", error))
+  }
+
   return (
     <section id="contact">
       <h1>Contact Me</h1>
@@ -26,15 +55,16 @@ export default function Contact() {
       </a>
       <div className="contact-form-container">
         <form
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
           className="contact-form"
           name="contact"
           method="post"
-          action="/success"
-          data-netlify-honeypot="bot-field"
-          data-netlify="true"
+          action="/"
+          onSubmit={handleSubmit}
         >
-          <input type="hidden" name="bot-field"></input>
           <input type="hidden" name="form-name" value="contact" />
+          <input type="hidden" name="bot-field"></input>
           <label htmlFor="contact-form-email">Email</label>
           <input
             id="contact-form-email"
@@ -42,6 +72,7 @@ export default function Contact() {
             type="email"
             name="email"
             required
+            onChange={({ target }) => setEmail(target.value)}
           />
           <label htmlFor="contact-form-name">Name</label>
           <input
@@ -50,6 +81,7 @@ export default function Contact() {
             type="text"
             name="name"
             required
+            onChange={({ target }) => setName(target.value)}
           />
           <label htmlFor="contact-form-message">Message</label>
           <textarea
@@ -60,6 +92,7 @@ export default function Contact() {
             rows="5"
             cols="50"
             required
+            onChange={({ target }) => setMessage(target.value)}
           />
           <button className="contact-form-submit" type="submit">
             Send <RiMailSendLine />
